423ffe93355068d35c2e41b0e2a347e9487d5495
Zhen Li <lizhen0425@gmail.com>
2016-04-05 23:29:56 -0500

    solution of P.282 added

---
+++ 282_Expression_Add_Operators.cpp
@@ -0,0 +1,94 @@
+//
+//  282_Expression_Add_Operators.cpp
+//  leetcode_cpp
+//
+//  Created by 李镇 on 4/2/16.
+//  Copyright © 2016 zl. All rights reserved.
+//
+
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> final = {};
+    
+    int calProduct(string& s, int k, int n){
+        string s_rtn = "";
+        int i = 0;
+        int j = 0;
+        int product = 1;
+        while(k > 0){
+            if(k % 2 == 0){
+                j++;
+            }else{
+                if(s[i] == '0' && j > i){
+                    return -1;
+                }
+                product *= stol(s.substr(i, j - i + 1));
+                s_rtn = s_rtn + s.substr(i, j - i + 1) + "*";
+                i = j + 1;
+                j = j + 1;
+            }
+            k /= 2;
+        }
+        
+        if(s[i] == '0' && n - i > 1){
+            return -1;
+        }
+        product *= stol(s.substr(i, n - i));
+        s_rtn = s_rtn + s.substr(i, n - i);
+        s = s_rtn;
+        return product;
+    }
+    
+    void DFS(string store, string remain, int target){
+        if(remain.length() == 0){
+            if(target == 0){
+                final.push_back(store.substr(0));
+            }
+            return;
+        }
+        
+
+        if(stol(remain) >= target && remain.length() >= 1){
+            for(int i = 1; i <= remain.length(); i++){
+                string mul = remain.substr(0, i);
+                string new_remain = remain.substr(i);
+                for(int j = 0; j < pow(2, i - 1); j++){
+                    string new_mul = mul;
+                    int product = calProduct(new_mul, j, i);
+                    if(product < 0){
+                        continue;
+                    }
+                    if(store != ""){
+                        DFS(store + "+" + new_mul, new_remain, target - product);
+                        DFS(store + "-" + new_mul, new_remain, target + product);
+                    }else{
+                        DFS(store + new_mul, new_remain, target - product);
+                    }
+                }
+            }
+        }
+    }
+    
+    vector<string> addOperators(string num, int target) {
+        final = {};
+        DFS("", num, target);
+        return final;
+    }
+};
+
+
+int main(void){
+    Solution ss;
+    vector<string> ans = ss.addOperators("123456789", 45);
+    for(auto k : ans){
+        cout << k << endl;
+    }
+    cout << "end" << endl;
+}
